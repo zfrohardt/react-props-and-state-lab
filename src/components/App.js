88f90f4d8dbbfce1350212cb5f7d1b7a2e392319
Zach Frohardt <zfrohardt@gmail.com>
2021-04-29 16:14:20 -0700

    commit tree 45c7628a48cd5bf062974bb633256bce093653a9

--- src/components/App.js
+++ src/components/App.js
@@ -24,16 +24,46 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.changeFilter} onFindPetsClick={this.fetchPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.adoptPet}/>
             </div>
           </div>
         </div>
       </div>
     )
   }
-}
 
+  fetchPets = () => {
+    let endpoint = '/api/pets';
+    let filter = this.state.filters.type;
+    if (filter !== 'all') {
+      endpoint = `${endpoint}?type=${filter}`;
+    }
+    fetch(endpoint).then(resp => resp.json())
+    .then(pets => this.setState({
+      pets: pets,
+    }));
+  }
+
+  changeFilter = filter => {
+    this.setState({
+      filters: {
+        type: filter
+      }
+    });
+  }
+
+  adoptPet = petId => {
+    this.setState({
+      pets: this.state.pets.map(pet => {
+        if (pet.id === petId) {
+          pet.isAdopted = true;
+        }
+        return pet;
+      })
+    })
+  }
+}
 export default App
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -6,7 +6,7 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={this.changeHandler}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,11 +15,21 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.submitHandler} >Find pets</button>
         </div>
       </div>
     )
   }
+
+  changeHandler = event => {
+    this.props.onChangeType(event.target.value);
+  }
+
+  submitHandler = event => {
+    this.props.onFindPetsClick();
+  }
+
+
 }
 
 export default Filters
